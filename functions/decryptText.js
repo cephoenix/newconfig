0ed4509f438093cfb4c4a2e762fe29b66b2c11f4
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-05-23 10:20:03 -0300

    a

--- functions/decryptText.js
+++ functions/decryptText.js
@@ -1,11 +1,34 @@
-exports = async function(arg){
+// exports = async function(arg){
   
-  let key = "603082712271C525E087BD999A4E0738";
-  let encryptedMessage;
-  if(typeof arg === "string") {
-      encryptedMessage = BSON.Binary.fromBase64(arg)
-  }
+//   let key = "603082712271C525E087BD999A4E0738";
+//   let encryptedMessage;
+//   if(typeof arg === "string") {
+//       encryptedMessage = BSON.Binary.fromBase64(arg)
+//   }
 
-  let encryptedText = utils.crypto.decrypt("aes", encryptedMessage, key);
-  return encryptedText.text();
+//   let encryptedText = utils.crypto.decrypt("aes", encryptedMessage, key);
+//   return encryptedText.text();
+// };
+
+exports = async function(salt, encoded){
+  const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+  const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
+  return encoded
+    .match(/.{1,2}/g)
+    .map((hex) => parseInt(hex, 16))
+    .map(applySaltToChar)
+    .map((charCode) => String.fromCharCode(charCode))
+    .join("");
 };
+
+
+// const decrypt = (salt, encoded) => {
+//   const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+//   const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
+//   return encoded
+//     .match(/.{1,2}/g)
+//     .map((hex) => parseInt(hex, 16))
+//     .map(applySaltToChar)
+//     .map((charCode) => String.fromCharCode(charCode))
+//     .join("");
+// };
--- functions/encryptText.js
+++ functions/encryptText.js
@@ -1,5 +1,31 @@
-exports = async function(arg){
-  let key = "603082712271C525E087BD999A4E0738";
-  let encryptedText = utils.crypto.encrypt("aes", arg, key);
-  return encryptedText.toBase64();
+// exports = async function(arg){
+//   let key = "603082712271C525E087BD999A4E0738";
+//   let encryptedText = utils.crypto.encrypt("aes", arg, key);
+//   return encryptedText.toBase64();
+// };
+
+// const crypt = (salt, text) => {
+//   const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+//   const byteHex = (n) => ("0" + Number(n).toString(16)).substr(-2);
+//   const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
+
+//   return text
+//     .split("")
+//     .map(textToChars)
+//     .map(applySaltToChar)
+//     .map(byteHex)
+//     .join("");
+// };
+
+exports = async function(salt, text){
+  const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+  const byteHex = (n) => ("0" + Number(n).toString(16)).substr(-2);
+  const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
+
+  return text
+    .split("")
+    .map(textToChars)
+    .map(applySaltToChar)
+    .map(byteHex)
+    .join("");
 };
