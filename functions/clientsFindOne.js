ba8a8e096e298cc66838e3659cc870af47b48363
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-05-23 17:28:09 -0300

    update clientsFindOne.js

--- functions/clientsFindOne.js
+++ functions/clientsFindOne.js
@@ -1,26 +1,26 @@
-exports = async function (data) {
-  const dbquery = context.services.get("mongodb-atlas").db("configRadio").collection("clients");
+// exports = async function (data) {
+//   const dbquery = context.services.get("mongodb-atlas").db("configRadio").collection("clients");
 
-  if (data) {
-    try {
-      parameters = EJSON.parse(data)
-    } catch (e) {
-      throw (e)
-    }
-  } else {
-    let err = new Error();
-    err.name = 'no_data_provided'
-    err.message = "Não é possível pesquisar por um registro em branco";
-    err.code = 3;
-    err.TypeError = 3;
-    throw err;
-  }
+//   if (data) {
+//     try {
+//       parameters = EJSON.parse(data)
+//     } catch (e) {
+//       throw (e)
+//     }
+//   } else {
+//     let err = new Error();
+//     err.name = 'no_data_provided'
+//     err.message = "Não é possível pesquisar por um registro em branco";
+//     err.code = 3;
+//     err.TypeError = 3;
+//     throw err;
+//   }
 
-  try {
-    dbResponse = await dbquery.findOne(parameters)
-  } catch (e) {
-    throw (e)
-  }
+//   try {
+//     dbResponse = await dbquery.findOne(parameters)
+//   } catch (e) {
+//     throw (e)
+//   }
 
-  return dbResponse;
-};
+//   return dbResponse;
+// };
