55fbc65ae66fc26dc1840e289ddcc63f62643068
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-05-23 11:06:11 -0300

    update encryptText.js

--- functions/decryptText.js
+++ functions/decryptText.js
@@ -11,6 +11,8 @@
 // };
 
 exports = async function(salt, encoded){
+  encoded = encoded.toLowerCase()
+  
   const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
   const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
   return encoded
