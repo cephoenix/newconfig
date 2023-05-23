1f4591f8dbe2e0c548a8dc29b3a8201026560c4d
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-05-23 09:36:56 -0300

    a

--- functions/encryptPassword.js
+++ functions/encryptPassword.js
@@ -1,8 +1,14 @@
 exports = async function(arg){
             // 603082712271C525E087BD999A4E0738
   let key = "B374A26A71490437AA024E4FADD5B497";
-  let encryptedText = utils.crypto.encrypt("aes", arg, key);
-  return encryptedText.toBase64();
+  // let encryptedText = utils.crypto.encrypt("aes", arg, key);
+  // return encryptedText.toBase64();
+  let encryptedPassword = utils.crypto.hash(
+    "sha1",
+    "key",
+  ).toHex().toUpperCase();
+  return crypto.createHash('sha256').update(arg).digest('base64')
+
 };
 
 
