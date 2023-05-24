695346caff9b8b722d55c78dd9c02f51779ce955
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-05-24 17:03:17 -0300

    update loginDoLogin.js

--- functions/loginDoLogin.js
+++ functions/loginDoLogin.js
@@ -17,10 +17,9 @@ exports = async function (data) {
   dbResponse = await context.functions.execute("usersFindOne", EJSON.stringify({ login: parameters.login }))
 
   if(dbResponse === null) {
-    return {debug: true}
     let err = new Error()
     err.message = "Usuário inexistente!"
-    throw err.message
+    throw err
   }
 
   let decryptedPassword = await context.functions.execute("decryptText", parameters.encryptedPassword) ///Decriptografa a senha e depois aplica o hash nela
@@ -29,7 +28,7 @@ exports = async function (data) {
   if (dbResponse.password !== hashedPass) {
     let err = new Error()
     err.message = "Senha incorreta!"
-    throw err.message
+    throw err
   }
 
   return { "sessionId": "A52B7A89FE6A3BA58D8C" }
