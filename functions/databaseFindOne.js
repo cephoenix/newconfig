11c9e8c7f30b0937649cdac33ba05b7f3f81ea1d
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-05-24 18:11:04 -0300

    update databaseFindOne.js

--- functions/databaseFindOne.js
+++ functions/databaseFindOne.js
@@ -1,20 +1,33 @@
 exports = async function (data) {
-
   let parameters;
-  
+
   if (data.query) {
     try {
       parameters = EJSON.parse(data.query)
     } catch (e) {
-      throw (e)
+      let err = new Error();
+      err.message = "Não foi possível buscar as informações no Banco de Dados. Favor conferir os critérios de busca!";
+      throw err;
     }
   } else {
     let err = new Error();
-    err.message = "Não é possível buscar por um registro em branco";
+    err.message = "É necessário informar os dados corretamente para fazer a pesquisa!";
+    throw err;
+  }
+
+  if (data.collection === undefined || data.collection === "" || data.collection === null) {
+    let err = new Error();
+    err.message = "É necessário informar uma collection para fazer a pesquisa!";
     throw err;
   }
 
   const dbquery = context.services.get("mongodb-atlas").db("configRadio").collection(data.collection);
+
+  if (data.parameters === undefined || data.collection === "" || data.collection === null) {
+    let err = new Error();
+    err.message = "É necessário informar pelo menos um parâmetro de busca para fazer a pesquisa!";
+    throw err;
+  }
 
   try {
     return await dbquery.findOne(parameters)
