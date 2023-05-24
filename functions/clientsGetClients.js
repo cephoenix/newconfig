3c628772acf7b5406fb02481c7c997c6bb640d6d
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-05-24 17:27:36 -0300

    update clientsGetClients.js

--- functions/clientsGetClients.js
+++ functions/clientsGetClients.js
@@ -10,13 +10,17 @@ exports = async function (data) {
     err.message = "Falha ao buscar o ID da sessão de usuário!"
     throw err
   }
-  
-  try {
-    return {debug: parameters}
-    dbResponse = await dbquery.find(parameters)
-  } catch (e) {
-    throw (e)
-  }
 
-  return dbResponse;
+  if(parameters.sessionId === "A52B7A89FE6A3BA58D8C") {
+    try {
+      dbResponse = await dbquery.find({})
+      return dbResponse;
+    } catch (e) {
+      throw (e)
+    }
+  } else {
+    let err = new Error()
+    err.message = "É necessário realizar login para ter acesso a esse recurso do sistema!"
+    throw err
+  }
 };
