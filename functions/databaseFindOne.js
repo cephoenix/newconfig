7b92fb1f5d8af95bfc705e0141d863bd5335d83f
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-05-25 09:43:37 -0300

    update databaseFindOne.js and loginDoLogin.js

--- functions/databaseFindOne.js
+++ functions/databaseFindOne.js
@@ -15,13 +15,6 @@ exports = async function (data) {
     throw "É necessário informar uma collection para fazer a pesquisa!";
   }
 
-  
-  if (data.parameters === undefined || data.collection === "" || data.collection === null) {
-    return {debug: {data: data, parameters: parameters}, moreDebug: {"1": data.parameters === undefined, "2": data.collection === "", "3": data.collection === null} }
-    throw "É necessário informar pelo menos um parâmetro de busca para fazer a pesquisa!";
-  }
-
-  
   const dbquery = context.services.get("mongodb-atlas").db("configRadio").collection(data.collection);
 
   try {
