395491a53400b26f1e6a35736fba4502104a122b
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-05-25 18:21:32 -0300

    update loginDoLogin.js

--- functions/loginDoLogin.js
+++ functions/loginDoLogin.js
@@ -16,7 +16,7 @@ exports = async function (data) {
     throw "É necessário fornecer informações válidas para autenticação! (3)"
   }
 
-  return {debug: data.text(), parameters: parameters}
+  return {debug: data.text(), login: parameters.login, encpass: parameters.encryptedPassword}
 
   try {
     dbResponse = await context.functions.execute('databaseFindOne', { query: EJSON.stringify({ login: parameters.login }), collection: "users" });
