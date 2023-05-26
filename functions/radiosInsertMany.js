34e8ce38e1f7009788d9ef9a41db9cbb689bab0b
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-05-26 14:18:49 -0300

    update radiosInsertMany.js

--- functions/radiosInsertMany.js
+++ functions/radiosInsertMany.js
@@ -5,6 +5,20 @@ exports = async function (data) {
   let query;
   let parameters;
 
+  if(data == undefined) {
+    throw "É necessário fornecer informações válidas para autenticação! (1)"
+  }
+  
+  if (data == null) {
+    throw "É necessário fornecer informações válidas para autenticação! (2)"
+  }
+
+  try {
+    parameters = JSON.parse(data.text())
+  } catch (e) {
+    throw "É necessário fornecer informações válidas para autenticação! (3)"
+  }
+
   const dbquery = context.services.get("mongodb-atlas").db("configRadio").collection("radios");
 
 return {debuga: true}
