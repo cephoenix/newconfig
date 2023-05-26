e1fc148702eba1c6d141e5c595003a2acfac5f49
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-05-26 14:34:52 -0300

    update radiosInsertMany.js

--- functions/radiosInsertMany.js
+++ functions/radiosInsertMany.js
@@ -6,73 +6,24 @@ exports = async function (data) {
   let parameters;
 
   if(data == undefined) {
-    throw "É necessário fornecer informações válidas para autenticação! (1)"
+    throw "É necessário fornecer informações válidas para inserir no Banco de Dados! (1)"
   }
   
   if (data == null) {
-    throw "É necessário fornecer informações válidas para autenticação! (2)"
+    throw "É necessário fornecer informações válidas para inserir no Banco de Dados! (2)"
   }
 
   try {
     parameters = JSON.parse(data.text())
   } catch (e) {
-    throw "É necessário fornecer informações válidas para autenticação! (3)"
+    throw "É necessário fornecer informações válidas para inserir no Banco de Dados! (3)"
   }
 
   const dbquery = context.services.get("mongodb-atlas").db("configRadio").collection("radios");
 
-
-
-  if (data) {
-    try {
-      parameters = EJSON.parse(data)
-    } catch (e) {
-      throw (e)
-    }
-  } else {
-    let err = new Error();
-    err.name = 'no_data_provided'
-    err.message = "Não é possível adicionar um registro em branco";
-    err.code = 2;
-    err.TypeError = 2;
-    throw err;
-  }
-
-  query = {
-    $or: [
-      { "address64Bits": parameters.address64Bits }
-    ]
-  }
-
   try {
-    dbResponse = await dbquery.findOne(query)
+    dbResponse = await dbquery.insertMany(parameters)
   } catch (e) {
-    let err = new Error();
-    err.name = 'find_one_error'
-    err.message = "Não é possível buscar rádio";
-    err.code = 2;
-    err.TypeError = 2;
-    err.e = e
-    throw e;
+    throw "Falha ao inserir dados no Banco de Dados!";
   }
-
-
-  if (!dbResponse) {
-    try {
-      dbResponse = await dbquery.insertOne(parameters);
-    } catch (e) {
-      throw "Erro: " + e;
-    }
-
-    return dbResponse
-
-  } else {
-    let err = new Error();
-    err.name = 'radio_already_exists'
-    err.message = "Rádio já cadastrado";
-    err.code = 1;
-    err.TypeError = 1;
-    throw err;
-  }
-
 };
