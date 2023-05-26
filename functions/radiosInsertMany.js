a1b5191ddd15ff1aa6c55a454f0ea46bed31c858
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-05-26 16:18:29 -0300

    update radiosInsertMany.js

--- functions/radiosInsertMany.js
+++ functions/radiosInsertMany.js
@@ -1,10 +1,8 @@
 exports = async function (data) {
 
   let dbResponse;
-  let resp = {};
-  let query;
   let parameters;
-
+return {debug3: data}
   if(data == undefined) {
     throw "É necessário fornecer informações válidas para inserir no Banco de Dados! (1)"
   }
