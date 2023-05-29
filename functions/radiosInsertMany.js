fb8e58dd84a14b2af18dbfc14c0c6526d3f4bf51
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-05-29 09:51:40 -0300

    update radiosInsertMany.js

--- functions/radiosInsertMany.js
+++ functions/radiosInsertMany.js
@@ -1,12 +1,7 @@
 exports = async function (data) {
-
-  let dbResponse;
-  let parameters;
-
   const dbquery = context.services.get("mongodb-atlas").db("configRadio").collection("radios");
-
   try {
-    dbResponse = await dbquery.insertMany(data, {ordered: false})
+    return await dbquery.insertMany(data, {ordered: false})
   } catch (e) {
     throw "Falha ao inserir dados no Banco de Dados!" + e;
   }
