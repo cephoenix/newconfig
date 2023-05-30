9c3310796a368993fee013bd73614a3afbb69c70
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-05-30 14:21:37 -0300

    update loginDoLogin.js

--- functions/loginDoLogin.js
+++ functions/loginDoLogin.js
@@ -2,10 +2,10 @@ exports = async function (data) {
   var parameters;
   var dbResponse;
 
-  if(data == undefined) {
+  if (data == undefined) {
     throw "É necessário fornecer informações válidas para autenticação! (1)"
   }
-  
+
   if (data == null) {
     throw "É necessário fornecer informações válidas para autenticação! (2)"
   }
@@ -16,29 +16,29 @@ exports = async function (data) {
     throw "É necessário fornecer informações válidas para autenticação! (3)"
   }
 
-  if(parameters.login == null || parameters.encryptedPassword == null) {
+  if (parameters.login == null || parameters.encryptedPassword == null) {
     throw "É necessário fornecer informações válidas para autenticação! (4)"
   }
 
-  if(parameters.login == undefined || parameters.encryptedPassword == undefined) {
+  if (parameters.login == undefined || parameters.encryptedPassword == undefined) {
     throw "É necessário fornecer informações válidas para autenticação! (5)"
   }
 
   try {
-    dbResponse = await context.functions.execute('databaseFindOne', 
-    { 
-      query: EJSON.stringify({ login: parameters.login }),
-      options:EJSON.stringify({ projection: {password: 0}} ), 
-      collection: "users"
-    });
+    dbResponse = await context.functions.execute('databaseFindOne',
+      {
+        query: EJSON.stringify({ login: parameters.login }),
+        options: EJSON.stringify({ projection: { _id: 0, password: 0 } }),
+        collection: "users"
+      });
   } catch (e) {
     throw "Erro ao buscar usuário no Banco de Dados! " + e
   }
 
-  if(dbResponse == null) {
+  if (dbResponse == null) {
     throw "Senha ou usuário incorretos!"
   }
-  
+
   let decryptedPassword = await context.functions.execute("decryptText", parameters.encryptedPassword) ///Decriptografa a senha e depois aplica o hash nela
   let hashedPass = await context.functions.execute("encryptPassword", decryptedPassword)
 
@@ -46,5 +46,5 @@ exports = async function (data) {
     throw "Senha ou usuário incorretos!"
   }
 
-  return { "sessionId": "A52B7A89FE6A3BA58D8C" , loggedUser: dbResponse}  //@todo implementar mecanismo de sessão
+  return { "sessionId": "A52B7A89FE6A3BA58D8C", loggedUser: dbResponse }  //@todo implementar mecanismo de sessão
 }
