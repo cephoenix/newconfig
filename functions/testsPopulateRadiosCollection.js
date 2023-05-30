6637f8c84ae71db950d2b9e57daa263116878f98
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-05-30 15:56:25 -0300

    update testsPopulateRadiosCollection.js

--- functions/testsPopulateRadiosCollection.js
+++ functions/testsPopulateRadiosCollection.js
@@ -31,7 +31,7 @@ exports = async function (data) {
       "clientType": "Desenvolvimento"
     });
   }
-
+  //forcing a new deploy
   try {
     dbResponse = await context.functions.execute('radiosInsertMany', parameters);
   } catch (e) {
