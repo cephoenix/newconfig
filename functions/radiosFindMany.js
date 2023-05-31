53d8f1623a3323079da96843184125a5215b0726
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-05-31 09:19:19 -0300

    update radiosFindMany.js

--- functions/radiosFindMany.js
+++ functions/radiosFindMany.js
@@ -13,7 +13,7 @@ exports = async function (data) {
   // }
 
   try {
-    dbResponse = await dbquery.find(data, {_id: 0, name: 1, firmwareVersion: 1, recordingDate: 1, clientChannel: 1})
+    dbResponse = await dbquery.find(data, {_id: 0, name: 1, firmwareVersion: 1, recordingDate: 1, clientChannel: 1, recordingDate: {"$dateToString": { format: "%H:%M:%S:%L%z", date: "$date", timezone: "America/Sao_Paulo"}}})
   } catch (e) {
     throw (e)
   }
