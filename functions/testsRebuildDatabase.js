06fdc44ccdbbb892650bf468faf0729f3d335fc1
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-05-31 17:33:36 -0300

    update testsRebuildDatabase.js

--- functions/testsRebuildDatabase.js
+++ functions/testsRebuildDatabase.js
@@ -144,7 +144,7 @@ exports = async function (data) {
     }]
   )
 
-  return dbResponse["insertedIds"]
+  return {dbResponse}.insertedIds
 
   context.services.get("mongodb-atlas").db("configRadio").collection("users").insertOne(
     {
