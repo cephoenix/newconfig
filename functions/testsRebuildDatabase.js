698ae44d42667a6adde84ddde2118a85fcd2ba5d
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-06-01 14:11:03 -0300

    update testsRebuildDatabase.js

--- functions/testsRebuildDatabase.js
+++ functions/testsRebuildDatabase.js
@@ -14,7 +14,7 @@ exports = async function (data) {
   })
 
   // The response body is a BSON.Binary object. Parse it and return.
-  return response.body
+  return JSON.parse(JSON.stringify(response.body))
   
 
   context.services.get("mongodb-atlas").db("configRadio").collection("clients").deleteMany({})
