ac9d9b2baaefb40e0bd925cf743425812b2e8ad3
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-06-01 17:38:55 -0300

    update loginDoLogin.js

--- functions/loginDoLogin.js
+++ functions/loginDoLogin.js
@@ -75,14 +75,14 @@ exports = async function (payload) {
   var rawData = JSON.parse(response.body.text()).response.results
 
   rawData.forEach(element => {
-    if(element.Nome.toUperCase().includes("LR")) {
+    // if(element.Nome.toUperCase().includes("LR")) {
       deviceTypes.push({
         productCode: element.Codigo,
         initials: element.SiglaConfRadio,
         class: element.DeviceClass,
         description: element.Nome
       })
-    }
+    // }
   });
 
   return { 
