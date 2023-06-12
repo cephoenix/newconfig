ae4e3eb84e897086684a35c3c1e6a0d7cc2be337
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-06-12 14:38:52 -0300

    update usersValidation.js

--- functions/usersValidation.js
+++ functions/usersValidation.js
@@ -92,7 +92,7 @@ async function validateCreate(payload) {
   } catch (e) {
     throw `Erro ao buscar usuário: ${e}`
   }
-throw {debug: JSON.stringify(dbResponse), moreDebug1: (dbResponse == undefined), moreDebug2: (dbResponse == ''), moreDebug3: (dbResponse == null), moreDebug4: (dbResponse == {})}
+throw {debug: JSON.stringify(dbResponse), moreDebug1: (dbResponse == undefined), moreDebug2: (dbResponse == ''), moreDebug3: (dbResponse == null), moreDebug4: (dbResponse == {}), moreDebug5: Object.keys(dbResponse).length}
   if(dbResponse != undefined && dbResponse != '' && dbResponse != null && dbResponse != {}) {
     throw `Esse usuário já existe!`
   }
