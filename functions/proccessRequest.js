323c5aaf26e909621dd4cb573b15265242945c41
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-06-13 11:28:25 -0300

    update proccessRequest.js

--- functions/proccessRequest.js
+++ functions/proccessRequest.js
@@ -3,12 +3,12 @@ exports = async function (payload) {
   // var headers
   // var query
   var body
-  // return {debug: payload.body, typeof: typeof payload.body, istrue: (typeof payload.body == 'undefined'), objectkeys: Object.keys(payload.body)}
+
   if(payload.body == undefined || payload.body == `` || payload.body == null || payload.body == 'undefined' || (typeof payload.body == 'undefined')) {
     body = {}
   } else {
     try {
-      
+      return {debug: payload.body, typeof: typeof payload.body, istrue: (typeof payload.body == 'undefined'), objectkeys: Object.keys(payload.body)}
       body = JSON.parse(payload.body.text())
     } catch (error) {
       throw `Erro ao processar corpo da requisição. Verifique os dados fornecidos! Erro: ${error}`
