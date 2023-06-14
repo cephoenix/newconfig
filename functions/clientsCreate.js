14e7088dfb01e2dafc3423aad891377e71882d5b
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-06-14 10:51:11 -0300

    update clientsControl.js, clientsCreate.js, clientsGetClients.js and clientsUpdateOne.js

--- functions/clientsCreate.js
+++ functions/clientsCreate.js
@@ -1,44 +1,44 @@
-exports = async function (data) {
+// exports = async function (data) {
 
-  let dbResponse;
-  let resp = {};
-  let query;
-  let parameters;
-  const dbquery = context.services.get("mongodb-atlas").db("configRadio").collection("clients");
+//   let dbResponse;
+//   let resp = {};
+//   let query;
+//   let parameters;
+//   const dbquery = context.services.get("mongodb-atlas").db("configRadio").collection("clients");
 
-  if (data) {
-    try {
-      parameters = EJSON.parse(data)
-    } catch (e) {
-      throw (e)
-    }
-  } else {
-    throw "Não é possível adicionar um registro em branco"
-  }
+//   if (data) {
+//     try {
+//       parameters = EJSON.parse(data)
+//     } catch (e) {
+//       throw (e)
+//     }
+//   } else {
+//     throw "Não é possível adicionar um registro em branco"
+//   }
 
-  // query = {
-  //   $or: [
-  //     { "initials": parameters.initials },
-  //     { "cpfCnpj": parameters.cpfCnpj },
-  //     { "networkKey": parameters.networkKey },
-  //     { "panId": parameters.panId }
-  //   ]
-  // }
+//   // query = {
+//   //   $or: [
+//   //     { "initials": parameters.initials },
+//   //     { "cpfCnpj": parameters.cpfCnpj },
+//   //     { "networkKey": parameters.networkKey },
+//   //     { "panId": parameters.panId }
+//   //   ]
+//   // }
 
-  // try {
-  //   dbResponse = await context.functions.execute('databaseFindOne', { query: EJSON.stringify(parameters), collection: "clients" });
-  // } catch (e) {
-  //   throw (e)
-  // }
+//   // try {
+//   //   dbResponse = await context.functions.execute('databaseFindOne', { query: EJSON.stringify(parameters), collection: "clients" });
+//   // } catch (e) {
+//   //   throw (e)
+//   // }
 
-  // if (!dbResponse) {
-    try {
-      dbResponse = await dbquery.insertOne(parameters);
-    } catch (e) {
-      throw e;
-    }
-    return dbResponse
-  // } else {
-  //   throw "Cliente já cadastrado"
-  // }
-};
+//   // if (!dbResponse) {
+//     try {
+//       dbResponse = await dbquery.insertOne(parameters);
+//     } catch (e) {
+//       throw e;
+//     }
+//     return dbResponse
+//   // } else {
+//   //   throw "Cliente já cadastrado"
+//   // }
+// };
--- functions/clientsUpdateOne.js
+++ functions/clientsUpdateOne.js
@@ -1,32 +1,32 @@
-exports = async function (data) {
+// exports = async function (data) {
 
-  let dbResponse;
-  let resp = {};
-  let query;
-  const dbquery = context.services.get("mongodb-atlas").db("configRadio").collection("clients");
+//   let dbResponse;
+//   let resp = {};
+//   let query;
+//   const dbquery = context.services.get("mongodb-atlas").db("configRadio").collection("clients");
 
-  if (data == null || data == "" || data == undefined) {
-    try {
-      parameters = EJSON.parse(data)
-    } catch (e) {
-      throw `Erro ao atualizar documento: ${e}`
-    }
-  } else {
-    throw "Não é possível atualizar um registro em branco"
-  }
+//   if (data == null || data == "" || data == undefined) {
+//     try {
+//       parameters = EJSON.parse(data)
+//     } catch (e) {
+//       throw `Erro ao atualizar documento: ${e}`
+//     }
+//   } else {
+//     throw "Não é possível atualizar um registro em branco"
+//   }
 
-  if(parameters._id == null || parameters._id == "" || parameters._id == undefined) {
-    throw "É necessário informar o ObjectId do documento a ser atualizado!"
-  }
+//   if(parameters._id == null || parameters._id == "" || parameters._id == undefined) {
+//     throw "É necessário informar o ObjectId do documento a ser atualizado!"
+//   }
 
-  // query = {"_id": new BSON.ObjectId(parameters._id)}
-  query = { "_id": parameters._id }
+//   // query = {"_id": new BSON.ObjectId(parameters._id)}
+//   query = { "_id": parameters._id }
 
-  try {
-    dbResponse = await dbquery.updateOne(query, parameters, { upsert: false })
-  } catch (e) {
-    throw `Erro ao atualizar documento: ${e}`
-  }
+//   try {
+//     dbResponse = await dbquery.updateOne(query, parameters, { upsert: false })
+//   } catch (e) {
+//     throw `Erro ao atualizar documento: ${e}`
+//   }
 
-  return dbResponse
-};
+//   return dbResponse
+// };
