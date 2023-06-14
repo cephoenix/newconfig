dd9513d59cf3ce68fb164083b8f4c5054aa9534f
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-06-14 12:04:00 -0300

    update databaseControl.js

--- functions/databaseControl.js
+++ functions/databaseControl.js
@@ -101,7 +101,7 @@ async function preproccess(parameters) {
   try {
     switch (parameters.action) {
       case 'findOne':
-        return {testando: parameters, boolean1: (parameters._id != null), boolean2: (parameters._id != undefined), boolean3: (parameters._id != ``)}
+        return {testando: parameters.query, boolean1: (parameters.query._id != null), boolean2: (parameters.query._id != undefined), boolean3: (parameters.query._id != ``)}
         if(parameters._id != null && parameters._id != undefined && parameters._id != ``) {
           parameters._id = new BSON.ObjectId(parameters._id)
         }
