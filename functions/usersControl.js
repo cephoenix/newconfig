5dfb850b9e4d42154f75ed84bb02297d3c42b872
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-06-15 17:39:41 -0300

    update usersControl.js

--- functions/usersControl.js
+++ functions/usersControl.js
@@ -180,7 +180,14 @@ async function blockUser(parameters) {
   } catch (error) {
     throw `Falha ao buscar usuário a ser bloqueado! ${error}`
   }
-  throw { blocked: userToBlock.blocked, a: (userToBlock.blocked != undefined), b: (userToBlock.blocked == true), c: (userToBlock.blocked == null), d: (userToBlock.blocked == ``), , user: userToBlock }
+  throw { 
+    blocked: userToBlock.blocked, 
+    a: (userToBlock.blocked != undefined), 
+    b: (userToBlock.blocked == true), 
+    c: (userToBlock.blocked == null), 
+    d: (userToBlock.blocked == ``), 
+    user: userToBlock 
+  }
   if(userToBlock.blocked != undefined && (userToBlock.blocked == true || userToBlock.blocked == null || userToBlock.blocked == ``)) { //Tem que verificar com undefined senão dá pau
     throw `Esse usuário já está bloqueado!`
   }
