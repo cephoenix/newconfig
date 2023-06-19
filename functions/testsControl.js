320d8e5ba970025f20250e2b03f930c676e82ccf
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-06-19 14:40:24 -0300

    update testsControl.js

--- functions/testsControl.js
+++ functions/testsControl.js
@@ -1,3 +1,4 @@
+import Utils from './utils.js';
 exports = async function (payload) {
   
   let action
@@ -25,17 +26,14 @@ exports = async function (payload) {
         operationParameters = null
         break;
       case 'debug':
-        
-        // const Utils = require('./utils.js');
-
-        // var u = new Utils();
+         var u = new Utils();
         
         // return {
         //   m1: u.metodo1(),
         //   m2: u.metodo2(), 
         //   debug: await context.services.get("mongodb-atlas").db("configRadio").collection(`radiosRecordingLog`).insertOne({debug: true, date: new Date().toLocaleString("pt-BR", {timeZone: "America/Sao_Paulo"})})
         // }
-        
+        return {debug:{}}
         break;
 
     default:
