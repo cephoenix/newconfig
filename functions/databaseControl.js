0ffd42e2c19eb2da56b3facbe30df255ec26ee75
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-06-19 10:05:33 -0300

    update databaseControl.js

--- functions/databaseControl.js
+++ functions/databaseControl.js
@@ -17,7 +17,7 @@ exports = async function(data){
   } catch (error) {
     throw `Falha ao executar pré-processamento dos dados a serem utilizados  na operação a ser efetuada no banco de dados! ${JSON.stringify(error)}`
   }
-  throw {debug: data}
+
   /**
    * Executa a operação no banco de dados
    * Nesse ponto os dados já devem ter sido validados e preparados para a operação
@@ -100,7 +100,7 @@ async function preproccess(parameters) {
  * @returns 
  */
 async function execute(parameters) {
-  
+  throw {debug: parameters}
   const dbquery = context.services.get("mongodb-atlas").db("configRadio").collection(parameters.collection)
   try {
     switch (parameters.action) {
