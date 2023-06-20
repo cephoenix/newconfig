f05315e1aded14c3d50e2c0d3f54641ca49ca022
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-06-20 17:17:34 -0300

    update testsControl.js

--- functions/testsControl.js
+++ functions/testsControl.js
@@ -7,14 +7,14 @@ exports = async function (payload) {
   let operationResponse
   let operationParameters
   let success = true
-/*
+
   try {
     //id, action, page etc should be on url parameters. These parameters are contained inside payload.query
     action = payload.query.action
   } catch (err) {
     action = payload.action
   }
-*/
+
   /**
    * Se tiver alguma verificação geral, que deve ser feita para todas as ações, ela deve ser feita aqui
    * Verificações específicas são feitas dentro de cada uma das operações
