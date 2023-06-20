c727acde19d11dac67978f826de66d3039ea6900
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-06-20 14:50:27 -0300

    update testsControl.js

--- functions/testsControl.js
+++ functions/testsControl.js
@@ -31,7 +31,7 @@ exports = async function (payload) {
         // ret.forEach(element => {
         //   temp.push(element)
         // });
-        return {ret: ret, temp: temp}
+        return {ret: ret, len: ret.length, pos1: ret[1]}
         // let databaseParameters = {
         //   action: `findMany`,
         //   collection: `clients`,
