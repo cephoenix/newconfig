c1388bb2ee3ca5e9642147bdd0d6a895aab80e24
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-06-21 10:53:13 -0300

    update radiosControl.js

--- functions/radiosControl.js
+++ functions/radiosControl.js
@@ -100,7 +100,7 @@ exports = async function (payload) {
       }
 
       let temp = await context.functions.execute(`databaseControl`, databaseParameters)
-      return {debug2: temp, params: p}
+      return {debug2: temp, params: databaseParameters}
       break;
 
     default:
