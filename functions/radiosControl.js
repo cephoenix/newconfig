8f8c81497a84048d51a0b2e80376f1dc97b7eb5f
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-06-21 11:23:47 -0300

    update radiosControl.js

--- functions/radiosControl.js
+++ functions/radiosControl.js
@@ -104,7 +104,7 @@ exports = async function (payload) {
       databaseParameters = {
         action: `findOne`,
         collection: `radios`,
-        query: { mac: processedRequestData.body.mac }
+        query: { address64Bit: processedRequestData.body.mac }
       }
 
       let device = await context.functions.execute(`databaseControl`, databaseParameters)
