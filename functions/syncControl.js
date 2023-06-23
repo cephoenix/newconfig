36d2b8609c8673b7045b0912437e00aa8c0e185f
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-06-23 15:48:45 -0300

    update syncControl.js

--- functions/syncControl.js
+++ functions/syncControl.js
@@ -7,6 +7,14 @@ exports = async function(payload){
     return { success: false, data: error}
   }
 
-  return {payload: payload, parsed: parsedInfo}
+  parsedInfo.array.forEach(element => {
+    const dbquery = context.services.get("mongodb-atlas").db("configRadio").collection(`clients`)
+    const dbquery2 = context.services.get("mongodb-atlas").db("configRadio").collection(`radiosRecordingLog`)
+    let client = dbquery.findOne({oldDatabaseId: element.id})
+    if(client == undefined || client == null || client == ``) {
+      dbquery2.insertOne(element)
+    }
+  });
+  return {parsed: parsedInfo}
 
 };
