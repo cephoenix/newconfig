8419fed7bb6a8a4b98bd8c0c5644b342dbcecd20
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-06-28 16:23:40 -0300

    update testsControl.js

--- functions/testsControl.js
+++ functions/testsControl.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // eslint-disable-next-line n/no-exports-assign
 exports = async function (payload) {
   let action
@@ -11,11 +12,11 @@ exports = async function (payload) {
   /**
     * Processa a requisição: Decodifica os dados e depois tranforma em formato JSON
     */
-  // try {
-  //   processedRequestData = await context.functions.execute('proccessRequest', payload)
-  // } catch (error) {
-  //   return { success: false, data: error }
-  // }
+  try {
+    processedRequestData = await context.functions.execute('proccessRequest', payload)
+  } catch (error) {
+    return { success: false, data: error }
+  }
 
   // processedRequestData =
   // {
