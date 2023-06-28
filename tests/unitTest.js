192e92797a4982e8fb3959c866461185ea81c9a1
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-06-28 18:53:24 -0300

    build(deps): update .package-lock.json, package-lock.json, package.json and unitTest.js

--- tests/unitTest.js
+++ tests/unitTest.js
@@ -1,51 +1,37 @@
-const test = require('../functions/testsControl.js')
+// import Realm from 'realm'
+const Realm = require('realm')
 
-test(
-  {
-    query: {
-      action: 'debug'
-    },
-    headers: {
-      'X-Envoy-External-Address': [
-        '200.181.33.155'
-      ],
-      'X-Forwarded-Client-Cert': [
-        'By=spiffe://xgen-prod/ns/baas-prod/sa/baas-main;Hash=c68c5aa61293af7317ce95a81111deb355d7f6acdfabeb775e95a468d14f947a;Subject=\"O=MongoDB\\, Inc.,CN=lb-b\";URI=spiffe://xgen-prod/ns/vm-prod/sa/lb-b'
-      ],
-      'User-Agent': [
-        'PostmanRuntime/7.32.3'
-      ],
-      'X-Forwarded-Proto': [
-        'https'
-      ],
-      'Content-Length': [
-        '30'
-      ],
-      'X-Request-Id': [
-        'ecae4330-2cf6-4870-9f75-343603e1af24'
-      ],
-      'X-Cluster-Client-Ip': [
-        '200.181.33.155'
-      ],
-      'Content-Type': [
-        'text/plain'
-      ],
-      Accept: [
-        '*/*'
-      ],
-      'Accept-Encoding': [
-        'gzip, deflate, br'
-      ],
-      'X-Forwarded-For': [
-        '200.181.33.155'
-      ],
-      'Postman-Token': [
-        '5fce5244-884c-4e12-96f5-62885e7626bd'
-      ]
-    },
-    body: {
-      Subtype: 0,
-      Data: 'ewogICAgImxvZ2luIjoiY2FybG9zZW1pbGlvIgp9'
-    }
+// const test = require('../functions/testsControl.js')
+
+const credentials = Realm.Credentials.apiKey('VRxu9sw1XokC8QZxbeNPiQujBzPuW4BKmGKXdaSfIMk4fG5iOalTp4Q6Y8Xx3FxX')
+
+async function login () {
+  try {
+    const app = new Realm.App({ id: 'application-0-xrtcd' })
+    const user = await app.logIn(credentials)
+    console.log('Usuário autenticado:', user)
+    const functions = user.functions
+
+    console.log('Debug: ', functions)
+    const result = await functions.encryptText('YTlhYWFiYWNhZGFlYWZhMA==')
+
+    console.log('Resultados: ', result)
+  } catch (error) {
+    console.error('Erro ao autenticar:', error)
   }
-)
+}
+
+async function init () {
+  try {
+    await login()
+    // // Obtém o cliente do banco de dados remoto
+    // const mongodb = app.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas')
+    // const users = mongodb.db('configRadio').collection('users')
+    // const doc = await users.findOne({ _id: '649c88e94c067236c8081adc' })
+    // console.log(`DEBUG: ${doc}`)
+  } catch (error) {
+    throw new Error('Problema ao realizar login')
+  }
+}
+
+init()
