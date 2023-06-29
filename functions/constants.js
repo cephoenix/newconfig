aae9f8454671f8ca598da05702798f1250dffa40
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-06-29 11:37:23 -0300

    test: update sync_metadata.realm and sync_metadata.realm.lock

---
+++ functions/constants.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line n/no-exports-assign
+exports = async function (data) {
+
+}
+
+if (typeof module === 'object') {
+  module.exports = exports
+}
