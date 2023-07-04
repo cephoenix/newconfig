58ceb707dee0cb2d446562ec5ab5c9ef39375808
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-07-04 09:06:46 -0300

    update radiosControl.js

--- functions/radiosControl.js
+++ functions/radiosControl.js
@@ -244,20 +244,20 @@ async function getRadioNumber (requestData) {
       // At this point definitive number should be the same as the one inside radio.clientSummary['<clientName>'] @todo checktit?
       // In fact this device should not need a network change....
     } else { // Device exists and its beeing changed to another network. We get a new number accordingly to client's summary (inside device object)
-      lastDeviceNumberForThisClient = device.clientSummary[`${client.initials}`]
-      if (lastDeviceNumberForThisClient != null && lastDeviceNumberForThisClient !== '') { // If we find a number for this device in clientSummary (Inside device object), we return it
-        definitiveNumber = lastDeviceNumberForThisClient
-      } else { // Otherwise this radio had never benn on this client network and we should get next number (based on last device of this network)
-        if (client.deviceSummary !== undefined && client.deviceSummary != null && client.deviceSummary !== '') { // if client summary is not null, use it to get device number (adding 1)
+      if (client.deviceSummary !== undefined && client.deviceSummary != null && client.deviceSummary !== '') { // if client summary is not null, we use it to get device number
+        lastDeviceNumberForThisClient = device.clientSummary[`${client.initials}`]
+        if (lastDeviceNumberForThisClient != null && lastDeviceNumberForThisClient !== '') { // If we find a number for this device in clientSummary (Inside device object), we return it
+          definitiveNumber = lastDeviceNumberForThisClient
+        } else { // Otherwise this radio had never been on this client network and we should get next number (based on last device of this network)
           if (lastDeviceNumber != null && lastDeviceNumber !== undefined && lastDeviceNumber !== '') { // If we find a log entry on deviceSmmary for this 'device type' we add 1 and return the new device number
             const lastDeviceNumber = +client.deviceSummary[`${deviceType}`]
             definitiveNumber = lastDeviceNumber + 1
           } else { // If we dont find any log entry on deviceSummary for this 'device type' we start from the beginning
             definitiveNumber = 1
           }
-        } else { // If clientSummay is null we also start from the beggining
-          definitiveNumber = 1
         }
+      } else { // If clientSummay is null we start from the beggining
+        definitiveNumber = 1
       }
     }
     ret.name = `${client.initials}_${deviceType}${String(definitiveNumber).padStart(4, '0')}` // Sets device name based on definitive number we got
