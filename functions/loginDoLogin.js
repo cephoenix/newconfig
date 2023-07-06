c926477c68e2b5fd983fecff895d67abbd472546
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-07-06 14:38:17 -0300

    update loginDoLogin.js

--- functions/loginDoLogin.js
+++ functions/loginDoLogin.js
@@ -1,76 +1,77 @@
 /* eslint-disable no-undef */
 // eslint-disable-next-line n/no-exports-assign
 exports = async function (payload) {
-  let parameters
-  let loggedUser
-  const data = payload.body
-  const remoteIp = payload.headers['X-Cluster-Client-Ip'][0]
+  return { success: false, data: deprecated }
+  // let parameters
+  // let loggedUser
+  // const data = payload.body
+  // const remoteIp = payload.headers['X-Cluster-Client-Ip'][0]
 
-  if (data == null) {
-    throw new Error('É necessário fornecer informações válidas para autenticação!')
-  }
+  // if (data == null) {
+  //   throw new Error('É necessário fornecer informações válidas para autenticação!')
+  // }
 
-  try {
-    parameters = JSON.parse(data.text())
-  } catch (e) {
-    throw new Error('É necessário fornecer informações válidas para autenticação!')
-  }
+  // try {
+  //   parameters = JSON.parse(data.text())
+  // } catch (e) {
+  //   throw new Error('É necessário fornecer informações válidas para autenticação!')
+  // }
 
-  if (parameters.login == null || parameters.encryptedPassword == null) {
-    throw new Error('É necessário fornecer informações válidas para autenticação!')
-  }
+  // if (parameters.login == null || parameters.encryptedPassword == null) {
+  //   throw new Error('É necessário fornecer informações válidas para autenticação!')
+  // }
 
-  try {
-    loggedUser = await context.services.get('mongodb-atlas').db('configRadio').collection('users').findOne({ login: parameters.login })
-  } catch (e) {
-    throw new Error(`Erro ao buscar usuário no Banco de Dados! ${e}`)
-  }
+  // try {
+  //   loggedUser = await context.services.get('mongodb-atlas').db('configRadio').collection('users').findOne({ login: parameters.login })
+  // } catch (e) {
+  //   throw new Error(`Erro ao buscar usuário no Banco de Dados! ${e}`)
+  // }
 
-  if (loggedUser == null) {
-    throw new Error('Senha ou usuário incorretos!')
-  }
+  // if (loggedUser == null) {
+  //   throw new Error('Senha ou usuário incorretos!')
+  // }
 
-  const decryptedPassword = await context.functions.execute('decryptText', parameters.encryptedPassword) // Decriptografa a senha e depois aplica o hash nela
-  const hashedPass = await context.functions.execute('encryptPassword', decryptedPassword)
+  // const decryptedPassword = await context.functions.execute('decryptText', parameters.encryptedPassword) // Decriptografa a senha e depois aplica o hash nela
+  // const hashedPass = await context.functions.execute('encryptPassword', decryptedPassword)
 
-  const dbquery = context.services.get('mongodb-atlas').db('configRadio').collection('usersLoginLog')
+  // const dbquery = context.services.get('mongodb-atlas').db('configRadio').collection('usersLoginLog')
 
-  if (loggedUser.password !== hashedPass) {
-    await dbquery.insertOne({ login: parameters.login, success: false, clientIp: remoteIp, date: new Date(), reason: 'Senha incorreta' })
-    throw new Error('Senha ou usuário incorretos!')
-  }
+  // if (loggedUser.password !== hashedPass) {
+  //   await dbquery.insertOne({ login: parameters.login, success: false, clientIp: remoteIp, date: new Date(), reason: 'Senha incorreta' })
+  //   throw new Error('Senha ou usuário incorretos!')
+  // }
 
-  if (loggedUser.blocked) {
-    await dbquery.insertOne({ login: parameters.login, success: false, clientIp: remoteIp, date: new Date(), reason: 'Usuário bloqueado' })
-    throw new Error('Usuário bloqueado!')
-  }
+  // if (loggedUser.blocked) {
+  //   await dbquery.insertOne({ login: parameters.login, success: false, clientIp: remoteIp, date: new Date(), reason: 'Usuário bloqueado' })
+  //   throw new Error('Usuário bloqueado!')
+  // }
 
-  await dbquery.insertOne({ login: parameters.login, success: true, clientIp: remoteIp, date: new Date() })
+  // await dbquery.insertOne({ login: parameters.login, success: true, clientIp: remoteIp, date: new Date() })
 
-  let databaseParameters = {
-    action: 'findMany',
-    collection: 'deviceTypes',
-    query: {},
-    filter: {}
-  }
+  // let databaseParameters = {
+  //   action: 'findMany',
+  //   collection: 'deviceTypes',
+  //   query: {},
+  //   filter: {}
+  // }
 
-  const deviceTypes = await context.functions.execute('databaseControl', databaseParameters)
+  // const deviceTypes = await context.functions.execute('databaseControl', databaseParameters)
 
-  databaseParameters = {
-    action: 'findOne',
-    collection: 'parameters',
-    query: { name: 'softwareVersion' },
-    filter: {}
-  }
+  // databaseParameters = {
+  //   action: 'findOne',
+  //   collection: 'parameters',
+  //   query: { name: 'softwareVersion' },
+  //   filter: {}
+  // }
 
-  const softwareVersion = await context.functions.execute('databaseControl', databaseParameters)
+  // const softwareVersion = await context.functions.execute('databaseControl', databaseParameters)
 
-  return {
-    sessionId: 'A52B7A89FE6A3BA58D8C',
-    loggedUser,
-    deviceTypes,
-    softwareVersion: softwareVersion.value
-  } // @todo implementar mecanismo de sessão
+  // return {
+  //   sessionId: 'A52B7A89FE6A3BA58D8C',
+  //   loggedUser,
+  //   deviceTypes,
+  //   softwareVersion: softwareVersion.value
+  // } // @todo implementar mecanismo de sessão
 }
 
 // /**
