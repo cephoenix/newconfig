374cd1f15fed47760ccb79f82c040960d67b6687
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-07-12 08:53:58 -0300

    update proccessUserAuthentication.js

--- functions/proccessUserAuthentication.js
+++ functions/proccessUserAuthentication.js
@@ -1,21 +1,23 @@
- exports = async function(authEvent) {
-
+/* eslint-disable no-undef */
+// eslint-disable-next-line n/no-exports-assign
+exports = async function (authEvent) {
   const {
-    user: { id },
-  } = authEvent;
+    user: { id }
+  } = authEvent
 
   const customUserData = context.services
-    .get("mongodb-atlas")
-    .db("configRadio")
-    .collection("customUserData");
-  const query = { _id: id };
+    .get('mongodb-atlas')
+    .db('configRadio')
+    .collection('customUserData')
+  const query = { _id: id }
   const update = {
     $set: {
       lastLogIn: new Date(),
+      custom: 'This is a custom user data... updated every time user logs on',
       debug: JSON.stringify(authEvent)
-    },
-  };
-  const options = { upsert: true };
-  await customUserData.updateOne(query, update, options);
+    }
+  }
+  const options = { upsert: true }
+  await customUserData.updateOne(query, update, options)
   context.user.custom_data.lastLogIn = new Date()
 }
