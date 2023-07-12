8135d3df9bd3420f28e29ba7035183b20a6589b4
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-07-12 08:57:09 -0300

    update debug.js

--- functions/debug.js
+++ functions/debug.js
@@ -1,8 +1,6 @@
-// This function is the endpoint's request handler.
-exports = function({ query, headers, body}, response) {
-
-  console.log("USER: ", JSON.stringify(context.user))
-
+/* eslint-disable no-undef */
+// eslint-disable-next-line n/no-exports-assign
+exports = function ({ query, headers, body }, response) {
+  console.log('USER: ', JSON.stringify(context.user))
   return context.user
-
-};
+}
