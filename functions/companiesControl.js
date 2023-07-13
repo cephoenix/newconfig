edcf0b0ad1e2639e39240ce89b9531f533a53580
Carlos Emílio Pereira <carlos.emilio@firebee.com.br>
2023-07-13 08:57:12 -0300

    update companiesControl.js

--- functions/companiesControl.js
+++ functions/companiesControl.js
@@ -23,5 +23,53 @@ exports = async function (payload) {
     case 'create':
       databaseAction = 'insertOne'
       break
+
+    case 'findOne':
+      databaseAction = 'findOne'
+      break
+
+    case 'findAll':
+      operationName = 'findMany'
+      operationParameters = {}
+      break
+
+    case 'findMany':
+      operationName = 'findMany'
+      if (payload.body == null) {
+        throw new Error('É necessário fornecer informações válidas para pesquisar no Banco de Dados!')
+      }
+
+      try {
+        operationParameters = JSON.parse(payload.body.text())
+      } catch (e) {
+        throw new Error('É necessário fornecer informações válidas (array) para pesquisar no Banco de Dados!')
+      }
+      break
+
+    case 'updateOne':
+      operationName = 'databaseUpdateOne'
+      break
+
+    case 'excludeOne':
+      operationName = 'databaseExcludeOne'
+      break
+
+    case 'deleteOne':
+      operationName = 'databaseDeleteOne'
+      break
+
+    case 'insertMany':
+
+      if (payload.body == null) {
+        throw new Error('É necessário fornecer informações válidas para inserir no Banco de Dados!')
+      }
+
+      try {
+        operationParameters = JSON.parse(payload.body.text())
+      } catch (e) {
+        throw new Error('É necessário fornecer informações válidas para inserir no Banco de Dados!')
+      }
+      operationName = 'radiosInsertMany'
+      break
   }
 }
